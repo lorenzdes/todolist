0c7ba0bc0e28c8c2e4fae2e5aa2b9f951998405b
lorenzdes <lorello97@gmail.com>
2024-06-13 11:58:13 +0200

    snackbar leaving page

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable no-unused-vars -->
 <template>
   <v-app>
     <v-app-bar app>
@@ -11,7 +12,34 @@
       <router-view></router-view>
     </v-main>
   </v-app>
+  <v-snackbar v-model="showSnackbar" :timeout="snackbarTimeout">
+    {{ snackbarMessage }}
+    <template v-slot:action>
+      <v-btn color="blue" text @click="showSnackbar = false"> Close </v-btn>
+    </template>
+  </v-snackbar>
 </template>
 
 <script setup>
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const showSnackbar = ref(false);
+const snackbarMessage = ref('');
+const snackbarTimeout = ref(2000); // 2 seconds
+
+// eslint-disable-next-line no-unused-vars
+let previousPageName = '';
+
+watch(
+  () => route.name,
+  (newValue, oldValue) => {
+    if (oldValue !== newValue) {
+      showSnackbar.value = true;
+      snackbarMessage.value = `You are leaving the ${oldValue} page!`;
+      previousPageName = oldValue;
+    }
+  }
+);
 </script>
