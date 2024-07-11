f96da9abc957b61a2995078681ad2795732f4768
lorenzdes <lorello97@gmail.com>
2024-07-11 10:10:42 +0200

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,26 @@
     <v-main>
       <router-view></router-view>
     </v-main>
+    <v-footer class="d-flex flex-column">
+    <div class="bg-teal d-flex w-100 align-center px-4">
+      <strong>Get connected with us on social networks!</strong>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        :icon="icon"
+        class="mx-4"
+        size="small"
+        variant="plain"
+      ></v-btn>
+    </div>
+
+    <div class="px-4 py-2 bg-black text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </div>
+  </v-footer>
     <v-snackbar v-model="showSnackbar" :timeout="snackbarTimeout">
       {{ snackbarMessage }}
       <template v-slot:action>
